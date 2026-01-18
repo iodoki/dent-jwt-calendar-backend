513ce053ba6107769b4abdae30d42238236274f0
Doklea Meci <codewithdoki@gmail.com>
2026-01-18 23:37:43 +0100

    doctor names

---
+++ src/main/resources/db/migration/V33__seed_update_doctor_entries.sql
@@ -0,0 +1,11 @@
+UPDATE doctors
+SET
+    first_name = 'Elda',
+    full_name = 'Dr.Elda Kola'
+Where id = '9fe2bfb7-47c6-40fb-a8a3-bc9922ef5b19';
+
+
+UPDATE doctors
+SET
+    full_name = 'Dr.Edison Kola'
+Where id = '29ec0998-e934-4180-9949-5d33de3efea3';
